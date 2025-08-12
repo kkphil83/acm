c8c62e310e82d33a508694c14a4f345fc21e76b1
kkphil83 <kkphil83@gmail.com>
2025-08-12 14:46:34 +0900

    fsc3

--- kueue/kueue-fairsharing/make-setup-kueue-fairsharing.sh
+++ kueue/kueue-fairsharing/make-setup-kueue-fairsharing.sh
@@ -1,6 +1,6 @@
 
-oc create -f ./team-a-ns.yaml -f ./team-b-ns.yaml
-oc create -f ./team-a-rb.yaml -f ./team-b-rb.yaml
+oc create -f ./team-a-ns.yaml -f ./team-b-ns.yaml -f ./team-c-ns.yaml
+oc create -f ./team-a-rb.yaml -f ./team-b-rb.yaml -f ./team-c-rb.yaml
 oc create -f ./default-flavor.yaml -f ./gpu-flavor.yaml
-oc create -f ./team-a-cq.yaml -f ./team-b-cq.yaml
-oc create -f ./team-a-local-queue.yaml -f ./team-b-local-queue.yaml
+oc create -f ./team-a-cq.yaml -f ./team-b-cq.yaml -f ./team-c-cq.yaml
+oc create -f ./team-a-local-queue.yaml -f ./team-b-local-queue.yaml -f ./team-c-local-queue.yaml
--- kueue/kueue-fairsharing/make-teardown-kueue-fairsharing.sh
+++ kueue/kueue-fairsharing/make-teardown-kueue-fairsharing.sh
@@ -1,11 +1,10 @@
 
-oc delete -f ./team-a-ray-cluster-prod.yaml -f ./team-b-ray-cluster-dev.yaml
-oc delete -f ./team-a-cq.yaml -f ./team-b-cq.yaml
-oc delete -f ./team-a-local-queue.yaml -f ./team-b-local-queue.yaml	
+oc delete -f ./team-a-ray-cluster-a.yaml -f ./team-b-ray-cluster-b.yaml -f ./team-c-ray-cluster-c.yaml
+oc delete -f ./team-a-cq.yaml -f ./team-b-cq.yaml -f ./team-c-cq.yaml
+oc delete -f ./team-a-local-queue.yaml -f ./team-b-local-queue.yaml	-f ./team-c-local-queue.yaml	
 oc delete -f ./default-flavor.yaml -f ./gpu-flavor.yaml
-oc delete -f ./team-a-rb.yaml -f ./team-b-rb.yaml
-oc delete -f ./team-a-ns.yaml -f ./team-b-ns.yaml
-oc delete -f ./workloadpriority.yml
+oc delete -f ./team-a-rb.yaml -f ./team-b-rb.yaml -f ./team-c-rb.yaml
+oc delete -f ./team-a-ns.yaml -f ./team-b-ns.yaml -f ./team-c-ns.yaml
 
 echo "Deleting all clusterqueues"
 oc delete clusterqueue --all --all-namespaces 
