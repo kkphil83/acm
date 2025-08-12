1f4746464b258bcb1b0c7ded83df48bb831ba048
kkphil83 <kkphil83@gmail.com>
2025-08-12 12:05:50 +0900

    11

--- kueue/kueue-fairsharing/make-teardown-kueue-fairsharing.sh
+++ kueue/kueue-fairsharing/make-teardown-kueue-fairsharing.sh
@@ -1,6 +1,6 @@
 
 oc delete -f ./team-a-ray-cluster-prod.yaml -f ./team-b-ray-cluster-dev.yaml
-oc delete -f ./team-a-cq.yaml -f ./team-b-cq.yaml -f ./shared-cq.yaml
+oc delete -f ./team-a-cq.yaml -f ./team-b-cq.yaml
 oc delete -f ./team-a-local-queue.yaml -f ./team-b-local-queue.yaml	
 oc delete -f ./default-flavor.yaml -f ./gpu-flavor.yaml
 oc delete -f ./team-a-rb.yaml -f ./team-b-rb.yaml
