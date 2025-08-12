670df367b4a2cda08b5824a5184776a01953467a
kkphil83 <kkphil83@gmail.com>
2025-08-12 10:19:19 +0900

    211

--- kueue/kueue-preemption/make-teardown-kueue-preemption.sh
+++ kueue/kueue-preemption/make-teardown-kueue-preemption.sh
@@ -7,9 +7,9 @@ oc delete -f ./team-a-rb.yaml -f ./team-b-rb.yaml
 oc delete -f ./team-a-ns.yaml -f ./team-b-ns.yaml
 oc delete -f ./workloadpriority.yml
 
-@echo "Deleting all clusterqueues"
+echo "Deleting all clusterqueues"
 oc delete clusterqueue --all --all-namespaces 
 
-@echo "Deleting all resourceflavors"
+echo "Deleting all resourceflavors"
 oc delete resourceflavor --all --all-namespaces 
 	
